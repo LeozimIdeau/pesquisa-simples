dacebd0e74e4ff5b30c208a9fe11f331d032b011
Leonardo Moraes <demoraesleonardo327@gmail.com>
2025-06-19 11:03:17 -0300

    Update manage.py

--- manage.py
+++ manage.py
@@ -6,21 +6,29 @@ def render():
           return render_template('menu.html')
 @app.route('/d',methods=['POST'])
 def dados():
+        generos=['mulher','homem','feminino','masculino','m','f']
+        genero=request.form.get('genero')
+        nome=request.form.get('nome')
+        insta=request.form.get('instagram')
+        idade=request.form.get('idade')
         dados={
 
-        'nome':request.form['nome'],
-        'genero':request.form['genero'],
-        'instagram':request.form['instagram'],
-        'idade':request.form['idade']
-}
-        try:
+                  'nome':nome,
+                  'genero':genero,
+                  'instagram':insta,
+                  'idade':idade
+                }
+        if genero in generos:
+            try:
                 with open ("dados/pesquisa.json","r") as arq:
                      lista_dados=json.load(arq)
-        except FileNotFoundError:
+            except FileNotFoundError:
                 lista_dados=[]
-        lista_dados.append(dados)
-        with open("dados/pesquisa.json","w") as arq:
+            lista_dados.append(dados)
+            with open("dados/pesquisa.json","w") as arq:
                 json.dump(lista_dados,arq,indent=4)
-        return render_template('resultados.html')
+            return render_template('resultados.html')
+        else:  
+                return render_template('menu.html')
 if __name__=='__main__':
         app.run(debug=True)
