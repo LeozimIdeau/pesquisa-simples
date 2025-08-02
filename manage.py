d84cf4c2db81686a946cdcf87f1e356da4d5c262
Leonardo Moraes <demoraesleonardo327@gmail.com>
2025-08-02 13:52:08 -0300

    Update manage.py

--- manage.py
+++ manage.py
@@ -1,16 +1,27 @@
 from flask import Flask,request,render_template,jsonify
 import json
 app=Flask(__name__)
+mulheres=0
+homens=0
 @app.route('/')
 def render():
           return render_template('menu.html')
 @app.route('/d',methods=['POST'])
 def dados():
-        generos=['mulher','homem','feminino','masculino','m','f','h']
+        global mulheres
+        global homens
+        generos=['mulher','homem','feminino','masculino','m','f']
+        generos_femininos=['mulher','feminino','f']
+        generos_masculinos=['homem','masculino','m']
         genero=request.form.get('genero')
         nome=request.form.get('nome')
         insta=request.form.get('instagram')
         idade=request.form.get('idade')
+        if genero in generos_femininos :
+              mulheres+=1
+        if genero in generos_masculinos:
+              homens+=1
+        total=mulheres+homens
         dados={
 
                   'nome':nome,
@@ -27,7 +38,7 @@ def dados():
             lista_dados.append(dados)
             with open("dados/pesquisa.json","w") as arq:
                 json.dump(lista_dados,arq,indent=4)
-            return render_template('resultados.html')
+            return render_template('resultados.html',mulheres=mulheres,homens=homens,total=total)
         else:  
             return render_template('menu.html')          
 if __name__=='__main__':
